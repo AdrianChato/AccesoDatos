6c6ddf33d16f8c022ecdc7f8e469d36b5f861373
AdrianChato <asolleo287@g.educaand.es>
2025-10-27 19:37:23 +0100

    TowerGPT

---
+++ TowerGPT/src/main/java/Modelos/InteraccionAgente.java
@@ -0,0 +1,81 @@
+package Modelos;
+
+public class InteraccionAgente {
+
+	private static int contador = 1;
+    private int identificador;
+    private TipoAgente tipo;
+    private String peticion;
+    private String respuesta;
+    private double tiempoEjecucion;
+    private int numValoracionesPositivas;
+    private double porcentajeAcierto;
+	public InteraccionAgente(TipoAgente tipo, String peticion, String respuesta,
+			double tiempoEjecucion, int numValoracionesPositivas, double porcentajeAcierto) {
+		super();
+		this.identificador = contador + contador++;
+		this.tipo = tipo;
+		this.peticion = peticion;
+		this.respuesta = respuesta;
+		this.tiempoEjecucion = tiempoEjecucion;
+		this.numValoracionesPositivas = numValoracionesPositivas;
+		this.porcentajeAcierto = porcentajeAcierto;
+	}
+	public static int getContador() {
+		return contador;
+	}
+	public static void setContador(int contador) {
+		InteraccionAgente.contador = contador;
+	}
+	public int getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+	public TipoAgente getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoAgente tipo) {
+		this.tipo = tipo;
+	}
+	public String getPeticion() {
+		return peticion;
+	}
+	public void setPeticion(String peticion) {
+		this.peticion = peticion;
+	}
+	public String getRespuesta() {
+		return respuesta;
+	}
+	public void setRespuesta(String respuesta) {
+		this.respuesta = respuesta;
+	}
+	public double getTiempoEjecucion() {
+		return tiempoEjecucion;
+	}
+	public void setTiempoEjecucion(double tiempoEjecucion) {
+		this.tiempoEjecucion = tiempoEjecucion;
+	}
+	public int getNumValoracionesPositivas() {
+		return numValoracionesPositivas;
+	}
+	public void setNumValoracionesPositivas(int numValoracionesPositivas) {
+		this.numValoracionesPositivas = numValoracionesPositivas;
+	}
+	public double getPorcentajeAcierto() {
+		return porcentajeAcierto;
+	}
+	public void setPorcentajeAcierto(double porcentajeAcierto) {
+		this.porcentajeAcierto = porcentajeAcierto;
+	}
+	@Override
+	public String toString() {
+		return "InteraccionAgente [identificador=" + identificador + ", tipo=" + tipo + ", peticion=" + peticion
+				+ ", respuesta=" + respuesta + ", tiempoEjecucion=" + tiempoEjecucion + ", numValoracionesPositivas="
+				+ numValoracionesPositivas + ", porcentajeAcierto=" + porcentajeAcierto + "]";
+	}
+	
+    
+    
+}
